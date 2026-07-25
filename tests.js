87f72e9c6ef5d6af6d6f538f8f36dffedb670c28
gerogergo <gerogergo1@gmail.com>
2026-07-25 02:21:48 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,1 +1,72 @@
+const tests = {
 
+
+"EGYENLET9":{
+
+
+title:
+"Egyenletek 9. osztály",
+
+
+time:
+10,
+
+
+questions:[
+
+
+{
+
+
+type:"radio",
+
+
+question:
+"Oldd meg: x²-5x+6=0",
+
+
+answers:[
+
+"x=2 és x=3",
+
+"x=-2 és x=-3",
+
+"x=1 és x=6",
+
+"Nincs megoldás"
+
+],
+
+
+correct:0
+
+
+},
+
+
+
+{
+
+
+type:"text",
+
+
+question:
+"Oldd meg: 2x+3=11",
+
+
+correct:"4"
+
+
+}
+
+
+
+]
+
+
+}
+
+
+
+};
